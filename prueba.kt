b79c89f2dece042c164379672fc8c5de1c883193
Victoria Ordorica Pardo <35547664+victoriaordoricapardo@users.noreply.github.com>
2023-03-17 18:02:37 -0600

    Add files via upload

---
+++ prueba.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val a: Int = 10
+    val b: Int
+    b = 20
+    var d: Int
+    d = 15
+    val e: Double = 3.14
+    var f: Array<String> = arrayOf("hello", "world")
+    val g: IntArray = intArrayOf(1, 2, 3)
+    val h: Boolean = true
+}
